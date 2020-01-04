f6ef6c92fbe1eae48a5e75a48474241a899a361b
eliezer bwana <eliezerbwana@gmail.com>
2020-01-05 00:33:31 +0200

    ajout des component acceuil,cours,compte. de la toolbar et du footer ainsi des links vers de nvx pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoursComponent } from "./cours/cours.component";
+import { AcceuilComponent } from "./acceuil/acceuil.component";
+import { CompteComponent } from "./compte/compte.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cours',component:CoursComponent},
+  {path:'acceuil',component:AcceuilComponent},
+  {path:'compte',component:CompteComponent},
+  {path:'',redirectTo:'/acceuil',pathMatch:'full'},
+  {path:'**',component:AcceuilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatMenuModule } from '@angular/material';
+  import { MatIconModule } from "@angular/material/icon";
+  import { RouterModule } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoursComponent } from './cours/cours.component';
+import { CompteComponent } from './compte/compte.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoursComponent,
+    CompteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
